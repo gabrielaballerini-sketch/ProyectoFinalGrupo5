01a10977134a3ebc2a06b39ec99a6d99f3c14591
gabrielaballerini-sketch <gabrielaballerini@gmail.com>
2025-10-18 18:14:27 -0300

    Inicio clases

--- src/modelo/Conexion.java
+++ src/modelo/Conexion.java
@@ -5,10 +5,47 @@
  */
 package modelo;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 /**
  *
  * @author Capotes
  */
 public class Conexion {
     
+   
+    private String usuario;
+    private String url;
+    private String password;
+    private static Connection conexion= null;
+    
+
+    public Conexion(String url, String usuario, String password) {
+        this.usuario = usuario;
+        this.url = url;
+        this.password = password;
+       
+    }
+      
+    public Connection buscarConexion () {
+        if(conexion ==null){
+            try {
+                Class.forName("org.mariadb.jdbc.Driver");
+                conexion = DriverManager.getConnection(url,usuario, password);
+            } catch (ClassNotFoundException ex) {
+                System.out.println("No es posible establecer connection");
+                //JOptionPane.showMessageDialog(null,"No es posible establecer la conexion...");
+            }catch (SQLException ex){
+                System.out.println("No es posible conectarse a la base de datos");
+                //JOptionPane.showMessageDialog(null, "Base de datos no encontrada...");
+                }
+            
+        }
+        return conexion;
+    }
 }
+    
+    
+

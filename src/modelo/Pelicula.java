e8f0f7b2d06e64e5d6bc20479a56ed345b8db8f6
gabrielaballerini-sketch <gabrielaballerini@gmail.com>
2025-10-19 16:08:23 -0300

    pelicula, sala

--- src/modelo/Pelicula.java
+++ src/modelo/Pelicula.java
@@ -5,10 +5,96 @@
  */
 package modelo;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Capotes
  */
 public class Pelicula {
+    
+    private String titulo;
+    private String director;
+    private String actores;
+    private String origen;
+    private String genero;
+    private LocalDate estreno;
+    private boolean enCartelera;
+
+    public Pelicula(String titulo, String director, String actores, String origen, String genero, LocalDate estreno, boolean enCartelera) {
+        this.titulo = titulo;
+        this.director = director;
+        this.actores = actores;
+        this.origen = origen;
+        this.genero = genero;
+        this.estreno = estreno;
+        this.enCartelera = enCartelera;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDirector() {
+        return director;
+    }
+
+    public void setDirector(String director) {
+        this.director = director;
+    }
+
+    public String getActores() {
+        return actores;
+    }
+
+    public void setActores(String actores) {
+        this.actores = actores;
+    }
+
+    public String getOrigen() {
+        return origen;
+    }
+
+    public void setOrigen(String origen) {
+        this.origen = origen;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public LocalDate getEstreno() {
+        return estreno;
+    }
+
+    public void setEstreno(LocalDate estreno) {
+        this.estreno = estreno;
+    }
+
+    public boolean isEnCartelera() {
+        return enCartelera;
+    }
+
+    public void setEnCartelera(boolean enCartelera) {
+        this.enCartelera = enCartelera;
+    }
+
+    @Override
+    public String toString() {
+        return "Pelicula{" + "titulo=" + titulo + ", director=" + director + ", actores=" + actores + ", origen=" + origen + ", genero=" + genero + ", estreno=" + estreno + ", enCartelera=" + enCartelera + '}';
+    }
+    
+    
+    
+    
+    
     
 }

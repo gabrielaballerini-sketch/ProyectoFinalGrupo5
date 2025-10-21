e29fd91543ae33344dac6081705ba88b9a07d56d
brian-delicia <briandaviddelicia@gmail.com>
2025-10-21 15:28:54 -0300

    conexion y comprador

--- src/modelo/Comprador.java
+++ src/modelo/Comprador.java
@@ -5,10 +5,81 @@
  */
 package modelo;
 
+import java.time.LocalDate;
+
 /**
  *
  * @author Capotes
  */
 public class Comprador {
+  
+            private int dni;
+            private String nombre;
+            private LocalDate fechaNac;
+            private String password;
+            private String medioDePago;
+            Conexion con  = new Conexion();
+
+    public Comprador(int dni, String nombre, LocalDate fechaNac, String password, String medioDePago) {
+        this.dni = dni;
+        this.nombre = nombre;
+        this.fechaNac = fechaNac;
+        this.password = password;
+        this.medioDePago = medioDePago;
+        con.buscarConexion();
+    }
+
+    public Comprador() {
+        con.buscarConexion();
+    }
+
+    public int getDni() {
+        return dni;
+    }
+
+    public void setDni(int dni) {
+        this.dni = dni;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public LocalDate getFechaNac() {
+        return fechaNac;
+    }
+
+    public void setFechaNac(LocalDate fechaNac) {
+        this.fechaNac = fechaNac;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getMedioDePago() {
+        return medioDePago;
+    }
+
+    public void setMedioDePago(String medioDePago) {
+        this.medioDePago = medioDePago;
+    }
+
+    @Override
+    public String toString() {
+        return "Comprador{" + "dni=" + dni + ", nombre=" + nombre + ", fechaNac=" + fechaNac + ", password=" + password + ", medioDePago=" + medioDePago + '}';
+    }
+    
+            
+            
+            
     
 }
--- src/modelo/Conexion.java
+++ src/modelo/Conexion.java
@@ -16,24 +16,26 @@ import java.sql.SQLException;
 public class Conexion {
     
    
-    private String usuario;
-    private String url;
-    private String password;
-    private static Connection conexion= null;
+    private static String usuario="root";
+    private static String url ="jdbc:mariadb://localhost:3306/proyectocinegrupo5";
+    private static String password = "";
+    private static Connection miConexion= null;
+
+    public Conexion() {
+        
+    }
     
 
-    public Conexion(String url, String usuario, String password) {
-        this.usuario = usuario;
-        this.url = url;
-        this.password = password;
-       
-    }
+ 
+
+   
+    
       
     public Connection buscarConexion () {
-        if(conexion ==null){
+        if(miConexion ==null){
             try {
                 Class.forName("org.mariadb.jdbc.Driver");
-                conexion = DriverManager.getConnection(url,usuario, password);
+                miConexion = DriverManager.getConnection(url,usuario, password);
             } catch (ClassNotFoundException ex) {
                 System.out.println("No es posible establecer connection");
                 //JOptionPane.showMessageDialog(null,"No es posible establecer la conexion...");
@@ -43,7 +45,7 @@ public class Conexion {
                 }
             
         }
-        return conexion;
+        return miConexion;
     }
 }
     

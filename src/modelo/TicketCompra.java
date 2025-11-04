424e8aaaea88036d3590c8e9e617062dc7b22a21
brian-delicia <briandaviddelicia@gmail.com>
2025-11-04 20:46:01 -0300

    retoques

--- src/modelo/TicketCompra.java
+++ src/modelo/TicketCompra.java
@@ -17,27 +17,27 @@ public class TicketCompra {
     private int id_ticket;
    private LocalDate fechaCompra;
     private int monto= 0;
-   private int dni;
-   private LocalDateTime horaInicio;
-   private int codLugar;
+   private Comprador comprador1;
+   private Funcion funcion1;
+   private Lugar lugar1  ;
     private String medioDePago;
 
-    public TicketCompra(LocalDate fechaCompra, int monto, int dni, LocalDateTime horaInicio, int codLugar,String medioPago) {
+    public TicketCompra(LocalDate fechaCompra, int monto, Comprador comprador1, Funcion funcion1,Lugar lugar1,String medioPago) {
         this.fechaCompra = fechaCompra;
         this.monto = monto;
-        this.dni = dni;
-        this.horaInicio = horaInicio;
-        this.codLugar = codLugar;
+        this.comprador1=comprador1;
+        this.funcion1=funcion1;
+        this.lugar1= lugar1;
         this.medioDePago= medioDePago;
     }
 
-    public TicketCompra(int id_ticket, LocalDate fechaCompra, int monto, int dni, LocalDateTime horaInicio, int codLugar) {
+    public TicketCompra(int id_ticket, LocalDate fechaCompra, int monto, Comprador comprador1, Funcion funcion1, Lugar lugar1) {
         this.id_ticket = id_ticket;
         this.fechaCompra = fechaCompra;
         this.monto = monto;
-        this.dni = dni;
-        this.horaInicio = horaInicio;
-        this.codLugar = codLugar;
+        this.comprador1=comprador1;
+        this.funcion1=funcion1;
+         this.lugar1= lugar1;
     }
 
     public int getId_ticket() {
@@ -72,29 +72,35 @@ public class TicketCompra {
         this.monto = monto;
     }
 
-    public int getDni() {
-        return dni;
+    public Comprador getComprador1() {
+        return comprador1;
     }
 
-    public void setDni(int dni) {
-        this.dni = dni;
+    public void setComprador1(Comprador comprador1) {
+        this.comprador1 = comprador1;
     }
 
-    public LocalDateTime getHoraInicio() {
-        return horaInicio;
+    public Funcion getFuncion1() {
+        return funcion1;
     }
 
-    public void setHoraInicio(LocalDateTime horaInicio) {
-        this.horaInicio = horaInicio;
+    public void setFuncion1(Funcion funcion1) {
+        this.funcion1 = funcion1;
     }
 
-    public int getCodLugar() {
-        return codLugar;
+   
+
+
+    public Lugar getLugar1() {
+        return lugar1;
     }
 
-    public void setCodLugar(int codLugar) {
-        this.codLugar = codLugar;
+    public void setLugar1(Lugar lugar1) {
+        this.lugar1 = lugar1;
     }
+
+   
+    
 
     public String getMedioDePago() {
         return medioDePago;
@@ -104,11 +110,16 @@ public class TicketCompra {
         this.medioDePago = medioDePago;
     }
 
-    @Override
-    public String toString() {
-        return "TicketCompra{" + "id_ticket=" + id_ticket + ", fechaCompra=" + fechaCompra + ", monto=" + monto + ", dni=" + dni + ", horaInicio=" + horaInicio + ", codLugar=" + codLugar + ", medioDePago=" + medioDePago + '}';
+    public TicketCompra(int id_ticket, LocalDate fechaCompra, Comprador comprador1, Funcion funcion1, Lugar lugar1, String medioDePago) {
+        this.id_ticket = id_ticket;
+        this.fechaCompra = fechaCompra;
+        this.comprador1 = comprador1;
+        this.funcion1 = funcion1;
+        this.lugar1 = lugar1;
+        this.medioDePago = medioDePago;
     }
 
+    
    
    
 
